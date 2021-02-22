256eb8aff15fd4badeca9d72b97a627a38ab87a2
yining wang <ywang@lcs.on.ca>
2021-02-21 21:22:07 -0500

    Initial Commit

---
+++ PoemAPI/PoemAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemAPIApp.swift
+//  PoemAPI
+//
+//  Created by Yining Wang on 2021-02-21.
+//
+
+import SwiftUI
+
+@main
+struct PoemAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
