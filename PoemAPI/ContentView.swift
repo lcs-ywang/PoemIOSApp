b56725e19f8d76dbbbc0367d495e4a3d2aa2edfd
yining wang <ywang@lcs.on.ca>
2021-02-21 21:31:07 -0500

    Fixed typo and syntax error

--- PoemAPI/ContentView.swift
+++ PoemAPI/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var poem:String
-    @State private var url:String, = "https://www.poemist.com/api/v1/randompoems"
+    @State private var url:String = "https://www.poemist.com/api/v1/randompoems"
     
     var body: some View {
         NavigationView{
@@ -35,7 +35,22 @@ struct ContentView: View {
             return
         }
         
-        
+//        URLSession.shared.dataTask(with: url) { data, response, error in // Getting data to other posts
+//                   if let data = data{
+//                       if let posts = try? JSONDecoder().decode(AllContentPostViewData.self, from: data){ // Data model, data input
+//                           // Update on the main thread
+//                           DispatchQueue.main.async {
+//                               self.posts = posts
+//                               self.sortedImageURL += seperate_image_urls(data: self.posts!.Posts)
+//                               self.sortedPost += seperate_data_from_data(data: self.posts!.Posts)
+//
+//                           }
+//                           print(self.sortedImageURL)
+//                           return
+//                       }
+//                   }
+//               }.resume()
+//        
     }
     
 }
