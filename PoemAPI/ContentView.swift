4e3c222da3ae0bdb47ee0c97813718219dd791f3
yining wang <ywang@lcs.on.ca>
2021-02-21 21:27:33 -0500

    Basic app layout

--- PoemAPI/ContentView.swift
+++ PoemAPI/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Form{
+                    Section(header:Text("Something ")){
+                        Text("Poem")
+                    }
+                    
+                    Section(header:Text("Something Else")){
+                        Text("Button")
+                    }
+                    
+                }
+                
+            }
+        }
+        .navigationBarTitle("Poem")
     }
 }
 
